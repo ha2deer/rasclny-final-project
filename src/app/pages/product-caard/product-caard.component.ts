d948417247b78e5da93e29b6a25117999697c5f0
Hadeer Mubarak <147769934+ha2deer@users.noreply.github.com>
2024-12-16 22:09:32 +0200

    Updated aboutus styles and added responsive section adjustments

--- src/app/pages/product-caard/product-caard.component.ts
+++ src/app/pages/product-caard/product-caard.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PointCartService} from "../../services/point-cart.service";
+import {products} from "../../types/dataType";
+import {CartService} from "../../services/cart.service";
+import {RouteTrackerService} from "../../services/route-tracker.service";
 
 @Component({
   selector: 'app-product-caard',
   templateUrl: './product-caard.component.html',
   styleUrls: ['./product-caard.component.css']
 })
-export class ProductCAardComponent {
+export class ProductCAardComponent implements OnInit {
+  products: any[] = []; // Array to hold all products
+  loading: boolean = true; // To show a loading indicator
+  productType: string = '';
 
+  constructor(private pointCartService: PointCartService, private cart: CartService, private routeTracker: RouteTrackerService) {}
 
+  ngOnInit(): void {
+    this.loadProducts();
+    const previousUrl = this.routeTracker.getPreviousUrl();
+    if (previousUrl?.includes('ponintreq')) {
+      this.productType = 'points';
+    } else if (previousUrl?.includes('category')) {
+      this.productType = 'category';
+    }
+  }
 
-
+  loadProducts(): void {
+    this.pointCartService.getData().subscribe({
+      next: (data) => {
+        this.products = data;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('Error loading products:', error);
+        this.loading = false;
+      },
+    });
+  }
+  onRequestProduct(event: MouseEvent, product: products): void {
+    event.stopPropagation()
+    this.cart.addToCart(product, 'points');
+    console.log('Product:', product);
+    console.log('Product Type:', 'points');
+  }
 }
